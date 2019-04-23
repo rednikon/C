e217b440a96e8cd7b8eab8f3e4482355d25d4da6
rednikon <30158376+rednikon@users.noreply.github.com>
2019-04-22 23:22:24 -0700

    Update main.c

--- For-Loops/main.c
+++ For-Loops/main.c
@@ -5,20 +5,20 @@
 
 int main(void)
 {
-    // define variables
+    //define variables
     int i=0;
     char x = '-', y = '|';
-    // first loop for top horizontal line
+    //first loop for top horizontal line
     for(i=0; i < 20; i++) {
         printf("%c", x);
     }
     printf("\n");
-    // loop for vertical lines
+    //loop for vertical lines
     for(i=0; i < 8; i++) {
         printf("%c                   %c", y, y);
         printf("\n");
     }
-    // bottom horizontal line
+    //bottom horizontal line
     for(i=0; i < 20; i++) {
         printf("%c", x);
     }
